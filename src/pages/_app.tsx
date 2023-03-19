00392c9976287ccd98d006f50aa03f5b799d2ed2
divopsor <divopsor@gmail.com>
2023-03-19 14:36:12 +0900

    fix: 빌드 오류 수정 [skip vercel]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import Footer from "@divops/component-footer";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Container>
+        <Component {...pageProps} />
+        <Footer />
+      </Container>
+    </QueryClientProvider>
+  );
 }
+
+const Container: Styled = ({ children }) => {
+  return (
+    <div
+      css={{
+        margin: "0 auto",
+        width: "1200px",
+      }}
+      children={children}
+    />
+  );
+};
